281655c7d455bf4b7919fabee22eed1bbfc62664
Son Chang <son@rancher.com>
2015-03-30 17:07:17 -0700

    check vnet and subnet

--- tests/validation/cattlevalidationtest/core/common_fixtures.py
+++ tests/validation/cattlevalidationtest/core/common_fixtures.py
@@ -42,6 +42,94 @@ def cleanup(client):
     delete_all(client, to_delete)
 
 
+def _admin_client():
+    return from_env(url=cattle_url(),
+                           cache=False,
+                           access_key='admin',
+                           secret_key='adminpass')
+
+
+def _client_for_user(name, accounts):
+    return from_env(url=cattle_url(),
+                           cache=False,
+                           access_key=accounts[name][0],
+                           secret_key=accounts[name][1])
+
+
+def create_user(admin_client, user_name, kind=None):
+    if kind is None:
+        kind = user_name
+
+    password = user_name + 'pass'
+    account = create_type_by_uuid(admin_client, 'account', user_name,
+                                  kind=user_name,
+                                  name=user_name)
+
+    active_cred = None
+    for cred in account.credentials():
+        if cred.kind == 'apiKey' and cred.publicValue == user_name \
+                and cred.secretValue == password:
+            active_cred = cred
+            break
+
+    if active_cred is None:
+        active_cred = admin_client.create_api_key({
+            'accountId': account.id,
+            'publicValue': user_name,
+            'secretValue': password
+        })
+
+    active_cred = wait_success(admin_client, active_cred)
+    if active_cred.state != 'active':
+        wait_success(admin_client, active_cred.activate())
+
+    return [user_name, password, account]
+
+
+def wait_success(client, obj, timeout=DEFAULT_TIMEOUT):
+    return client.wait_success(obj, timeout=timeout)
+
+
+def create_type_by_uuid(admin_client, type, uuid, activate=True, validate=True,
+                        **kw):
+    opts = dict(kw)
+    opts['uuid'] = uuid
+
+    objs = admin_client.list(type, uuid=uuid)
+    obj = None
+    if len(objs) == 0:
+        obj = admin_client.create(type, **opts)
+    else:
+        obj = objs[0]
+
+    obj = wait_success(admin_client, obj)
+    if activate and obj.state == 'inactive':
+        obj.activate()
+        obj = wait_success(admin_client, obj)
+
+    if validate:
+        for k, v in opts.items():
+            assert getattr(obj, k) == v
+
+    return obj
+
+@pytest.fixture(scope='session')
+def accounts():
+    result = {}
+    admin_client = _admin_client()
+    for user_name in ['admin', 'agent', 'user', 'agentRegister', 'test',
+                      'readAdmin', 'token', 'superadmin', 'service']:
+        result[user_name] = create_user(admin_client,
+                                        user_name,
+                                        kind=user_name)
+
+    result['admin'] = create_user(admin_client, 'admin')
+    system_account = admin_client.list_account(kind='system', uuid='system')[0]
+    result['system'] = [None, None, system_account]
+
+    return result
+
+
 @pytest.fixture(scope='session')
 def client(cattle_url):
     client = from_env(url=cattle_url)
@@ -54,6 +142,11 @@ def admin_client(cattle_url):
     admin_client = from_env(url=cattle_url)
     assert admin_client.valid()
     return admin_client
+
+@pytest.fixture(scope='session')
+def super_client(request, accounts):
+    ret = _client_for_user('superadmin', accounts)
+    return ret
 
 
 @pytest.fixture
