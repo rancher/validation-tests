0900f86afce3cea8df319d1c7de83cf680afc095
Arunkumar Eli <elrarun@gmail.com>
2016-01-12 12:02:23 -0800

    Added few replica test cases

--- tests/validation_v2/storage/common_fixtures.py
+++ tests/validation_v2/storage/common_fixtures.py
@@ -1,17 +1,6 @@
-import pytest
-import random
-import requests
 import os
-import sys
-import time
 import logging
-import paramiko
-import inspect
-import re
-from docker import Client
-import pickle
 
-# PYTHONPATH="$PYTHONPATH:/usr/local/rancher/libs/block:/usr/local/rancher/protobuf" python2.7 /usr/local/rancher/cli/replica.py
 CONTROLLER = os.environ.get('CONTROLLER_HOST')
 REPLICA1 = os.environ.get('REPLICA1')
 REPLICA2 = os.environ.get('REPLICA2')
@@ -36,51 +25,3 @@ FORMAT = "\n[ %(asctime)s %(levelname)s %(filename)s:%(lineno)s " \
 logging.basicConfig(level=logging.INFO, format=FORMAT,
                     datefmt='%a, %d %b %Y %H:%M:%S')
 logger = logging.getLogger(__name__)
-
-
-@pytest.fixture(scope='session')
-def get_ssh_handlers():
-
-    controller = os.environ.get('CONTROLLER_HOST')
-    replica1 = os.environ.get('REPLICA1')
-    replica2 = os.environ.get('REPLICA2')
-    i = 1
-    #
-    # Try to connect to the host.
-    # Retry a few times if it fails.
-    #
-    while True:
-        logger.info("Trying to connect to %s (%i/30)", controller, i)
-        try:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            privatekeyfile = os.path.expanduser('~/.ssh/arun_do_pvt_key')
-            mykey = paramiko.RSAKey.from_private_key_file(privatekeyfile)
-            ssh.connect(controller, username='root', pkey=mykey)
-            logger.info("Connected to %s", controller)
-            break
-        except paramiko.AuthenticationException:
-            logger.info("Authentication failed when connecting to %s",
-                        controller)
-            sys.exit(1)
-        except:
-            logger.info("Could not SSH to %s, waiting for it to start",
-                        controller)
-            i += 1
-            time.sleep(2)
-
-        # If we could not connect within time limit
-        if i == 30:
-            logger.info("Could not connect to %s. Giving up. "
-                        "Please check private key file.", controller)
-            ssh.close()
-            sys.exit(1)
-    try:
-        cmd = "sudo docker ps"
-        # Send the command (non-blocking)
-        logger.info("command being executed %s:", cmd)
-        stdin, stdout, stderr = ssh.exec_command(cmd)
-        response = stdout.readlines()
-        logger.info(response)
-    except:
-        logger.info("Execution of cmd %s failed", cmd)
