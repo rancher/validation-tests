f3733ab1bdba0cdaadec75b81f1fed3427ec6fda
Sangeetha Hariharan <sangeetha@rancher.com>
2017-02-06 15:50:21 -0800

    Added LB and services with link to the initial stack deployments

--- tests/v2_validation/cattlevalidationtest/core/test_network_policy.py
+++ tests/v2_validation/cattlevalidationtest/core/test_network_policy.py
@@ -3,7 +3,8 @@ from common_fixtures import *  # NOQA
 test_network_policy = os.environ.get(
     'TEST_NETWORK_POLICY', "False")
 np_reason = \
-    'Rancher NetworkPolicy Manager needs to de deployed in "Default" env'
+    'Intended to not execute network policy test'
+
 if_network_policy = pytest.mark.skipif(test_network_policy != "True",
                                        reason=np_reason)
 
@@ -13,20 +14,18 @@ NETWORKPOLICY_SUBDIR = \
 policy_within_stack = {"within": "stack", "action": "allow"}
 policy_groupby = {"between": {"groupBy": "com.rancher.stack.location"},
                   "action": "allow"}
-
-if_compose_data_files = pytest.mark.skipif(
-    not os.path.isdir(NETWORKPOLICY_SUBDIR),
-    reason='Docker compose files directory location not set/ does not Exist')
-
 shared_environment = {"env": []}
 
 
 @pytest.fixture(scope='session', autouse=True)
 def create_env_for_network_policy(request, client):
-    env1 = create_stack_with_service(client, "stack1.yml", "stack1-rc.yml")
-    assert len(env1.services()) == 4
-    env2 = create_stack_with_service(client, "stack2.yml", "stack2-rc.yml")
+    assert check_for_network_policy_manager(client)
+    env2 = create_stack_with_service(client, "test2", NETWORKPOLICY_SUBDIR,
+                                     "stack2.yml", "stack2-rc.yml")
     assert len(env2.services()) == 4
+    env1 = create_stack_with_service(client, "test1", NETWORKPOLICY_SUBDIR,
+                                     "stack1.yml", "stack1-rc.yml")
+    assert len(env1.services()) == 8
     shared_environment["env"].append(env1)
     shared_environment["env"].append(env2)
     shared_environment["env1_test1allow"] = \
@@ -37,12 +36,23 @@ def create_env_for_network_policy(request, client):
         get_service_by_name(client, env1,  "test3deny")
     shared_environment["env1_test4deny"] = \
         get_service_by_name(client, env1,  "test4deny")
+    shared_environment["env1_lbwithininstack"] = \
+        get_service_by_name(client, env1,  "lbwithininstack")
+    shared_environment["env1_lbcrossstack"] = \
+        get_service_by_name(client, env1,  "lbcrossstack")
+    shared_environment["env1_servicewithlinks"] = \
+        get_service_by_name(client, env1,  "servicewithlinks")
+    shared_environment["env1_servicecrosslinks"] = \
+        get_service_by_name(client, env1,  "servicecrosslinks")
+
     shared_environment["env2_test1allow"] = \
         get_service_by_name(client, env2,  "test1allow")
     shared_environment["env2_test2allow"] = \
         get_service_by_name(client, env2,  "test2allow")
     shared_environment["env2_test3deny"] = \
         get_service_by_name(client, env2,  "test3deny")
+    shared_environment["env2_test4deny"] = \
+        get_service_by_name(client, env2,  "test4deny")
     shared_environment["env2_test4deny"] = \
         get_service_by_name(client, env2,  "test4deny")
 
@@ -69,6 +79,23 @@ def validate_default_network_action_deny_networkpolicy_allow_within_stacks(
          shared_environment["env2_test3deny"],
          shared_environment["env2_test4deny"]],
         connection="deny")
+    validate_lb_service(admin_client, admin_client,
+                        shared_environment["env1_lbwithininstack"],
+                        "9091",
+                        [shared_environment["env1_test1allow"]])
+    validate_linked_service(admin_client,
+                            shared_environment["env1_servicewithlinks"],
+                            [shared_environment["env1_test1allow"]],
+                            "99")
+    # Cross stacks access should be denied
+    validate_linked_service(admin_client,
+                            shared_environment["env1_servicecrosslinks"],
+                            [shared_environment["env2_test2allow"]],
+                            "98", linkName="test2allow.test2",
+                            not_reachable=True)
+    validate_lb_service_for_no_access(admin_client,
+                                      shared_environment["env1_lbcrossstack"],
+                                      "9090")
 
 
 def validate_default_network_action_deny_networkpolicy_none(
@@ -84,6 +111,20 @@ def validate_default_network_action_deny_networkpolicy_none(
          shared_environment["env2_test3deny"],
          shared_environment["env2_test4deny"]],
         connection="deny")
+    validate_lb_service_for_no_access(
+        admin_client, shared_environment["env1_lbwithininstack"], "9091")
+    validate_linked_service(admin_client,
+                            shared_environment["env1_servicewithlinks"],
+                            [shared_environment["env1_test1allow"]],
+                            "99", not_reachable=True)
+    validate_linked_service(admin_client,
+                            shared_environment["env1_servicecrosslinks"],
+                            [shared_environment["env2_test2allow"]],
+                            "98", linkName="test2allow.test2",
+                            not_reachable=True)
+    validate_lb_service_for_no_access(admin_client,
+                                      shared_environment["env1_lbcrossstack"],
+                                      "9090")
 
 
 def validate_default_network_action_deny_networkpolicy_groupby(
@@ -233,27 +274,6 @@ def test_dna_deny_np_groupby_restart_service(
         admin_client)
 
 
-def create_stack_with_service(client, dc_file, rc_file):
-    random_name = random_str()
-    env_name = random_name.replace("-", "")
-    dockerCompose = readDataFile(NETWORKPOLICY_SUBDIR, dc_file)
-    rancherCompose = readDataFile(NETWORKPOLICY_SUBDIR, rc_file)
-    env = client.create_stack(name=env_name,
-                              dockerCompose=dockerCompose,
-                              rancherCompose=rancherCompose,
-                              startOnCreate=True)
-    env = client.wait_success(env, timeout=300)
-    assert env.state == "active"
-
-    for service in env.services():
-        wait_for_condition(
-            client, service,
-            lambda x: x.state == "active",
-            lambda x: 'State is: ' + x.state,
-            timeout=60)
-    return env
-
-
 def set_network_policy(client, defaultPolicyAction="allow", policy=None):
     networks = client.list_network(name='ipsec')
     assert len(networks) == 1
@@ -262,3 +282,14 @@ def set_network_policy(client, defaultPolicyAction="allow", policy=None):
         network, defaultPolicyAction=defaultPolicyAction, policy=policy)
     network = wait_success(client, network)
     assert network.defaultPolicyAction == defaultPolicyAction
+
+
+def check_for_network_policy_manager(client):
+    np_manager = False
+    env = client.list_stack(name="network-policy-manager")
+    if len(env) == 1:
+        service = get_service_by_name(client, env[0],
+                                      "network-policy-manager")
+        if service.state == "active":
+            np_manager = True
+    return np_manager
