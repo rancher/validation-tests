74c80d7cafa5909bf58db0d3652cffc45dc978e6
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-07-12 14:52:00 +0200

    Add sleep again

--- tests/v2_validation/cattlevalidationtest/core/test_stress.py
+++ tests/v2_validation/cattlevalidationtest/core/test_stress.py
@@ -185,6 +185,7 @@ def test_deploy_k8s_yaml(kube_hosts):
         "port_ext": "1"
     }
     create_stack(input_config)
+    time.sleep(120)
     validate_stack(input_config)
 
 
@@ -205,11 +206,13 @@ def test_upgrade_validate_k8s(kube_hosts):
         validate_kubectl()
         assert check_k8s_dashboard()
         modify_stack(input_config)
+        time.sleep(120)
         # New stack
         input_config = {
             "namespace": "stresstest-ns-"+str(i),
             "port_ext": str(i)
         }
         create_stack(input_config)
+        time.sleep(120)
         validate_stack(input_config)
         assert validate_helm()
