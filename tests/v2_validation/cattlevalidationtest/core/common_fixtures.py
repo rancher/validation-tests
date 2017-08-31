7fe0dce54b3663f3958b1cc04f7072e1c30bdee6
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2017-08-31 04:59:07 +0200

    Add quotes to avoid spaces problems

--- tests/v2_validation/cattlevalidationtest/core/common_fixtures.py
+++ tests/v2_validation/cattlevalidationtest/core/common_fixtures.py
@@ -757,9 +757,9 @@ def generate_socat_certificates(hosts):
     hosts_names = ",".join(hosts_names)
     os.environ["SAN"] = hosts_names
     cmd = 'openssl req -new -x509 -days 365 -nodes' + \
-        ' -out ' + SSLCERT_SUBDIR + '/socat-crt.pem' + \
-        ' -keyout ' + SSLCERT_SUBDIR + '/socat-key.pem' + \
-        ' -config ' + SSLCERT_SUBDIR + '/san-env.conf -extensions san_env'
+        ' -out "' + SSLCERT_SUBDIR + '/socat-crt.pem"' + \
+        ' -keyout "' + SSLCERT_SUBDIR + '/socat-key.pem"' + \
+        ' -config "' + SSLCERT_SUBDIR + '/san-env.conf" -extensions san_env'
     logger.info(cmd)
     os.system(cmd)
     os.system(
