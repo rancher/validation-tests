8da7c5e30dbd9c79a07d2e74c74f7fe3db664348
moelsayed <m.elsayed@gmail.com>
2017-09-11 20:30:44 +0200

    add k8s stress tests, forced stack upgrade and additional needed code

--- tests/v2_validation/cattlevalidationtest/core/common_fixtures.py
+++ tests/v2_validation/cattlevalidationtest/core/common_fixtures.py
@@ -11,7 +11,9 @@ import inspect
 import re
 import json
 import base64
+import jinja2
 import docker
+
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -119,6 +121,7 @@ rancher_compose_con = {"container": None, "host": None, "port": "7878"}
 kubectl_client_con = {"container": None, "host": None, "port": "9999"}
 rancher_cli_con = {"container": None, "host": None, "port": "7879"}
 kubectl_version = os.environ.get('KUBECTL_VERSION', "v1.4.6")
+helm_version = os.environ.get('HELM_VERSION', "v2.1.3")
 CONTAINER_STATES = ["running", "stopped", "stopping"]
 check_connectivity_by_wget = True
 cert_list = {}
@@ -134,6 +137,12 @@ api_version = "v2-beta"
 sleep_interval = int(os.environ.get('CATTLE_SLEEP_INTERVAL', 5))
 restart_sleep_interval = \
     int(os.environ.get('CATTLE_RESTART_SLEEP_INTERVAL', 10))
+
+k8s_base_lb_port = os.environ.get("BASE_LB_PORT", "88")
+k8s_base_external_port = os.environ.get("BASE_EXTERNAL_PORT", "300")
+k8s_base_node_port = os.environ.get("BASE_NODE_PORT", "310")
+k8s_base_ingress_port = os.environ.get("BASE_INGRESS_PORT", "8")
+k8s_base_lb_node_port = os.environ.get("BASE_LB_NODE_PORT", "320")
 
 
 @pytest.fixture(scope='session')
@@ -2782,7 +2791,13 @@ def create_kubectl_client_container(client, port,
     cmd3 = "mkdir .kube"
     cmd4 = "echo '" + kube_config + "'> .kube/config"
     cmd5 = "./kubectl version"
-    cmd = cmd1 + ";" + cmd2 + ";" + cmd3 + ";" + cmd4 + ";" + cmd5
+    cmd6 = "wget https://storage.googleapis.com/kubernetes-helm/helm-" + \
+           helm_version + "-linux-amd64.tar.gz"
+    cmd7 = "tar xfz helm-" + helm_version + "-linux-amd64.tar.gz " + \
+           "--strip-components 1"
+    cmd8 = "./helm version"
+    cmd = "" + cmd1 + ";" + cmd2 + ";" + cmd3 + ";" + cmd4 + ";" + cmd5 + \
+          ";" + cmd6 + ";" + cmd7 + ";" + cmd8
     stdin, stdout, stderr = ssh.exec_command(cmd)
     response = stdout.readlines()
     ssh.close()
@@ -2839,6 +2854,32 @@ def execute_kubectl_cmds(command, expected_resps=None, file_name=None,
                 found = True
                 print "Found in Error Response " + err_str
         assert found
+    return str_response
+
+
+def execute_helm_cmds(command, chdir=None, expected_resps=None):
+    cmd = "./helm " + command
+    if chdir:
+        cmd = "cd " + chdir + ";" + cmd
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(
+        kubectl_client_con["host"].ipAddresses()[0].address, username="root",
+        password="root", port=int(kubectl_client_con["port"]))
+    print cmd
+
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    response = stdout.readlines()
+    error = stderr.readlines()
+    str_error = ""
+    for err in error:
+        str_error += err
+    print "Error in response" + str(str_error)
+
+    str_response = ""
+    for resp in response:
+        str_response += resp
+    print "Obtained Response: " + str_response
     return str_response
 
 
@@ -4167,3 +4208,404 @@ def delete_volume(client, volume):
     assert volume.state == "removed"
     volume = client.wait_success(volume.purge())
     assert volume.state == "purged"
+
+
+# k8s related fixtures
+def k8s_create_stack(input_config):
+    namespace = input_config["namespace"]
+    create_ns(namespace)
+
+    # Create pre upgrade resources
+    get_response = execute_kubectl_cmds("get nodes -o json")
+    nodes = json.loads(get_response)
+    node1 = nodes['items'][0]['status']['addresses'][0]['address']
+
+    # Render the testing yaml
+    input_config["external_node"] = node1
+    input_config["k8s_base_lb_port"] = k8s_base_lb_port
+    input_config["k8s_base_external_port"] = k8s_base_external_port + "0"
+    input_config["k8s_base_node_port"] = k8s_base_node_port + "0"
+    input_config["k8s_base_lb_node_port"] = k8s_base_lb_node_port + "0"
+    input_config["k8s_base_ingress_port"] = k8s_base_ingress_port
+    if len(input_config["port_ext"]) > 1:
+        input_config["k8s_base_external_port"] = k8s_base_external_port
+        input_config["k8s_base_node_port"] = k8s_base_node_port
+        input_config["k8s_base_lb_node_port"] = k8s_base_lb_node_port
+    fname = os.path.join(K8_SUBDIR, "upgrade_testing.yml.j2")
+    rendered_tmpl = jinja2_render(fname, input_config)
+
+    with open(os.path.join(K8_SUBDIR, "upgrade_testing.yml"), "wt") as fout:
+        fout.write(rendered_tmpl)
+    fout.close()
+    execute_kubectl_cmds(
+        "create --namespace="+namespace,
+        file_name="upgrade_testing.yml")
+
+
+def k8s_validate_stack(input_config):
+    namespace = input_config["namespace"]
+    lb_port = int(k8s_base_lb_port + input_config["port_ext"])
+    external_port = k8s_base_external_port + "0" + input_config["port_ext"]
+    node_port = int(k8s_base_node_port + "0" + input_config["port_ext"])
+    if len(input_config["port_ext"]) > 1:
+        external_port = k8s_base_external_port + input_config["port_ext"]
+        node_port = int(k8s_base_node_port + input_config["port_ext"])
+    ingress_port = k8s_base_ingress_port + input_config["port_ext"]
+
+    get_response = execute_kubectl_cmds("get nodes -o json")
+    nodes = json.loads(get_response)
+    node1 = nodes['items'][0]['status']['addresses'][0]['address']
+    # Verify the nginx pod is created
+    waitfor_pods(selector="app=nginx-pod", namespace=namespace, number=1)
+    get_response = execute_kubectl_cmds(
+        "get pod/nginx-pod -o json --namespace="+namespace)
+
+    pod = json.loads(get_response)
+    assert pod['metadata']['name'] == "nginx-pod"
+    assert pod['kind'] == "Pod"
+    assert pod['status']['phase'] == "Running"
+    container = pod['status']['containerStatuses'][0]
+    assert "husseingalal/nginx-curl" in container['image']
+    assert container['restartCount'] == 0
+    assert container['ready']
+    assert container['name'] == "nginx"
+
+    # Verify RC is created
+    get_response = execute_kubectl_cmds(
+        "get rc/nginx -o json --namespace="+namespace)
+    rc = json.loads(get_response)
+    assert rc["metadata"]["name"] == "nginx"
+    assert rc["metadata"]["labels"]["name"] == "nginx"
+    assert rc["spec"]["replicas"] == 2
+    assert rc["spec"]["selector"]["name"] == "nginx"
+    container = rc["spec"]["template"]["spec"]["containers"][0]
+    assert "sangeetha/testnewhostrouting" in container["image"]
+    assert container["name"] == "nginx"
+    waitfor_pods(
+        selector="type=rc", namespace=namespace, number=2)
+    get_response = execute_kubectl_cmds(
+        "get pod --selector=type=rc"
+        " -o json --namespace="+namespace)
+    pod = json.loads(get_response)
+    assert len(pod["items"]) == 2
+    pods_list = []
+    for pod in pod["items"]:
+        pods_list.append(pod["metadata"]["name"])
+        assert pod["metadata"]["labels"]["name"] == "nginx"
+        assert pod["metadata"]["namespace"] == namespace
+        container = pod["spec"]["containers"][0]
+        assert "sangeetha/testnewhostrouting" in container["image"]
+        assert container["name"] == "nginx"
+        assert pod["status"]["phase"] == "Running"
+
+    # Verify that the Load Balancer service is working
+    get_response = execute_kubectl_cmds(
+        "get service nginx-lb -o json --namespace="+namespace)
+    service = json.loads(get_response)
+    assert service['metadata']['name'] == "nginx-lb"
+    assert service['kind'] == "Service"
+    assert service['spec']['ports'][0]['port'] == lb_port
+    assert service['spec']['ports'][0]['protocol'] == "TCP"
+    time.sleep(20)
+    get_response = execute_kubectl_cmds(
+        "get service nginx-lb -o json --namespace=" + namespace)
+    service = json.loads(get_response)
+    lbip = service['status']['loadBalancer']['ingress'][0]["ip"]
+    time.sleep(20)
+    check_round_robin_access_k8s_service(pods_list, lbip, str(lb_port),
+                                         path="/name.html")
+
+    # Verify that the external service is working
+    check_round_robin_access_k8s_service(pods_list, node1, str(external_port),
+                                         path="/name.html")
+
+    # Verify that the Clusterip service is working
+    get_response = execute_kubectl_cmds(
+        "get service nginx-clusterip -o json --namespace="+namespace)
+    service = json.loads(get_response)
+    assert service['metadata']['name'] == "nginx-clusterip"
+    assert service['kind'] == "Service"
+    assert service['spec']['ports'][0]['port'] == 8000
+    assert service['spec']['ports'][0]['protocol'] == "TCP"
+    clusterip = service['spec']['clusterIP']
+    clusterport = service['spec']['ports'][0]['port']
+    get_response = execute_kubectl_cmds(
+        "get pod --selector=app=nginx-pod -o json --namespace="+namespace)
+    pods = json.loads(get_response)
+    clusterurl = clusterip+":"+str(clusterport)
+    nginxpod = pods['items'][0]['metadata']['name']
+
+    cmd_result = k8s_execute_cmd(
+        nginxpod,
+        '''curl -s -w "%{http_code}" ''' + clusterurl + " -o /dev/null",
+        namespace)
+    cmd_result = cmd_result.rstrip()
+    assert cmd_result == "200"
+
+    # Verify that the nodeport service is working
+    get_response = execute_kubectl_cmds(
+        "get service nodeport-nginx -o json --namespace="+namespace)
+    service = json.loads(get_response)
+    assert service['metadata']['name'] == "nodeport-nginx"
+    assert service['kind'] == "Service"
+    assert service['spec']['ports'][0]['nodePort'] == node_port
+    assert service['spec']['ports'][0]['port'] == 80
+    assert service['spec']['ports'][0]['protocol'] == "TCP"
+    get_response = execute_kubectl_cmds(
+        "get pod --selector=name=nginx -o json --namespace="+namespace)
+    pods = json.loads(get_response)
+    get_response = execute_kubectl_cmds("get nodes -o json")
+    nodes = json.loads(get_response)
+    for node in nodes["items"]:
+        node_ip = node['status']['addresses'][0]['address']
+        check_round_robin_access_k8s_service(pods_list, node_ip,
+                                             str(node_port), path="/name.html")
+
+    # Check if the ingress works
+    ingress_name = "ingress1"
+    port = ingress_port
+
+    # Initial set up
+    lbips = wait_for_ingress_to_become_active(ingress_name, namespace, 1)
+
+    selector1 = "k8s-app=k8test1-service"
+    pod_new_names = get_pod_names_for_selector(selector1, namespace, scale=1)
+
+    check_round_robin_access_lb_ip(pod_new_names, lbips[0], port,
+                                   hostheader="foo.bar.com",
+                                   path="/service3.html")
+
+    check_round_robin_access_lb_ip(["nginx-ingress2"], lbips[0], port,
+                                   hostheader="foo.bar.com",
+                                   path="/name.html")
+
+
+def k8s_modify_stack(input_config):
+    namespace = input_config["namespace"]
+    ingress_port = k8s_base_ingress_port + input_config["port_ext"]
+
+    # Scale the RC
+    get_response = execute_kubectl_cmds(
+        "scale rc nginx --replicas=3 --namespace="+namespace)
+    get_response = execute_kubectl_cmds(
+        "get rc/nginx -o json --namespace="+namespace)
+    rc = json.loads(get_response)
+    assert rc["metadata"]["name"] == "nginx"
+    assert rc["metadata"]["labels"]["name"] == "nginx"
+    assert rc["spec"]["replicas"] == 3
+    assert rc["spec"]["selector"]["name"] == "nginx"
+    container = rc["spec"]["template"]["spec"]["containers"][0]
+    assert "sangeetha/testnewhostrouting" in container["image"]
+    assert container["name"] == "nginx"
+    waitfor_pods(
+        selector="type=rc", namespace=namespace, number=3)
+    get_response = execute_kubectl_cmds(
+        "get pod --selector=type=rc"
+        " -o json --namespace="+namespace)
+    pod = json.loads(get_response)
+    assert len(pod["items"]) == 3
+    for pod in pod["items"]:
+        assert pod["metadata"]["labels"]["name"] == "nginx"
+        assert pod["metadata"]["namespace"] == namespace
+        container = pod["spec"]["containers"][0]
+        assert "sangeetha/testnewhostrouting" in container["image"]
+        assert container["name"] == "nginx"
+        assert pod["status"]["phase"] == "Running"
+
+    # Check if the ingress works
+    ingress_name = "ingress1"
+    port = ingress_port
+
+    lbips = wait_for_ingress_to_become_active(ingress_name, namespace, 1)
+    selector1 = "k8s-app=k8test1-service"
+    rc_name1 = "k8testrc1"
+    get_response = execute_kubectl_cmds(
+        "scale rc "+rc_name1+" --replicas=3 --namespace="+namespace)
+    waitfor_pods(selector=selector1, namespace=namespace, number=3)
+    pod_new_names = get_pod_names_for_selector(selector1, namespace, scale=3)
+
+    # Check if the ingress works with the new pods
+    ingress_name = "ingress1"
+    time.sleep(20)
+    check_round_robin_access_lb_ip(pod_new_names, lbips[0], port,
+                                   hostheader="foo.bar.com",
+                                   path="/service3.html")
+
+
+def k8s_execute_cmd(pod, cmd, namespace):
+    result = execute_kubectl_cmds(
+                "exec " + pod + " --namespace=" + namespace + " -- " + cmd)
+    return result
+
+
+def jinja2_render(tpl_path, context):
+    path, filename = os.path.split(tpl_path)
+    return jinja2.Environment(
+        loader=jinja2.FileSystemLoader(path)
+    ).get_template(filename).render(context)
+
+
+def k8s_check_dashboard():
+    k8s_client = kubectl_client_con["k8s_client"]
+    project_id = k8s_client.list_project()[0].id
+    dashboard_url = rancher_server_url() + \
+        '/r/projects/' + \
+        project_id + \
+        '/kubernetes-dashboard:9090/'
+    try:
+        r = requests.get(dashboard_url)
+        r.close()
+        return r.ok
+    except requests.ConnectionError:
+        logger.info("Connection Error - " + dashboard_url)
+        return False
+
+
+def k8s_force_upgrade_stack(stack_name):
+    k8s_client = kubectl_client_con["k8s_client"]
+    access_key = k8s_client._access_key
+    secret_key = k8s_client._secret_key
+
+    force_up_commands = [
+        "export RANCHER_URL=" + rancher_server_url(),
+        "export RANCHER_ACCESS_KEY=" + access_key,
+        "export RANCHER_SECRET_KEY=" + secret_key,
+        "export RANCHER_ENVIRONMENT=" + PROJECT_ID,
+        "cd rancher-v*",
+        "./rancher export " + stack_name,
+        "./rancher up --force-upgrade --confirm-upgrade " +
+        "-d -s " + stack_name + " --batch-size 1 " +
+        "--interval 1000 -f " + stack_name + "/docker-compose.yml" +
+        " --rancher-file=" + stack_name + "/rancher-compose.yml"
+    ]
+
+    logger.info("Final command: " + " ; ".join(force_up_commands))
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(
+            rancher_cli_con["host"].ipAddresses()[0].address, username="root",
+            password="root", port=int(rancher_cli_con["port"]))
+    except Exception as e:
+        logger.info("SSH Connection Error")
+        raise e
+    stdin, stdout, stderr = ssh.exec_command(
+                                            " ; ".join(force_up_commands),
+                                            timeout=1200)
+    response = stdout.readlines()
+    error = stderr.readlines()
+    logger.info(response)
+    logger.info(error)
+
+    env = k8s_client.list_stack(name=stack_name)
+    assert len(env) == 1
+    environment = env[0]
+    wait_for_condition(
+        k8s_client, environment,
+        lambda x: x.healthState == "healthy",
+        lambda x: 'State is: ' + x.healthState,
+        timeout=1200)
+
+
+def k8s_waitfor_infra_stacks():
+    k8s_client = kubectl_client_con["k8s_client"]
+    infra_stacks = [
+            "ipsec",
+            "network-services",
+            "healthcheck",
+            "kubernetes"
+            ]
+    for stack_name in infra_stacks:
+        env = k8s_client.list_stack(name=stack_name)
+        assert len(env) == 1
+        environment = env[0]
+        wait_for_condition(
+            k8s_client, environment,
+            lambda x: x.healthState == "healthy",
+            lambda x: 'State is: ' + x.healthState,
+            timeout=1200)
+
+
+def k8s_validate_kubectl():
+    # make sure that kubectl is working
+    started = time.time()
+    while time.time() - started < 1200:
+        while True:
+            try:
+                get_response = execute_kubectl_cmds("get nodes -o json")
+                break
+            except:
+                time.sleep(2)
+                continue
+        nodes = json.loads(get_response)
+        if len(nodes['items']) == kube_host_count:
+            ready_flag = True
+            for node in nodes['items']:
+                logger.info("node name: " + str(node['metadata']['name']))
+                if node['status']['conditions'][3]['status'] != "True":
+                    ready_flag = False
+            if ready_flag:
+                logger.info("hosts found after: " + str(time.time() - started))
+                return True
+        time.sleep(2)
+    return False
+
+
+def k8s_validate_helm():
+    response = execute_helm_cmds("create test-nginx")
+    print response
+    get_response = execute_helm_cmds("install test-nginx \
+        --name stresstest --namespace stresstest-ns --replace")
+
+    if "STATUS: DEPLOYED" not in get_response:
+        print "dies at install"
+        return False
+    time.sleep(10)
+
+    get_response = execute_kubectl_cmds(
+                    "get svc stresstest-test-nginx --namespace \
+                    stresstest-ns -o json")
+    print get_response
+    service = json.loads(get_response)
+    assert service['metadata']['name'] == "stresstest-test-nginx"
+
+    waitfor_pods(
+        selector="app=stresstest-test-nginx",
+        namespace="stresstest-ns", number=1)
+    get_response = execute_kubectl_cmds(
+        "get pods -o json -l 'app=stresstest-test-nginx'  ")
+    pod = json.loads(get_response)
+
+    for pod in pod["items"]:
+        assert pod["status"]["phase"] == "Running"
+        assert pod['kind'] == "Pod"
+
+    # Remove the release
+    response = execute_helm_cmds("delete --purge stresstest")
+    print response
+    time.sleep(10)
+    response = execute_helm_cmds("ls -q stresstest")
+    assert response is ''
+    return True
+
+
+def k8s_check_cluster_health(input_config=None):
+    # k8s initial checks
+    assert k8s_validate_kubectl()
+    assert k8s_check_dashboard()
+    assert k8s_validate_helm()
+    # create a test stack if you don't get one.
+    if not input_config:
+        input_config = {
+            "namespace": "stresstest-ns-0",
+            "port_ext": "0"
+        }
+        k8s_create_stack(input_config)
+        time.sleep(120)
+
+    # do test stack related tests
+    k8s_validate_stack(input_config)
+    k8s_modify_stack(input_config)
+    time.sleep(120)
